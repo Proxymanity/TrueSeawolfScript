6e91a70fd93d7f3f9c5eddb7fdc6a413aff4e9c4
proxymanity <cK2701**>
2016-10-31 01:37:39 -0400

    Numbers(add, subtract, mult, divide, mod) work. Working on strings

--- seawolf_parser.py
+++ seawolf_parser.py
@@ -8,25 +8,27 @@ global_variables = dict()
 
 precedence = (
     ('left', 'PLUS', 'MINUS'),
-    ('left', 'TIMES', 'DIVIDE', 'MODULO')
+    ('left', 'MULT', 'DIVIDE', 'MODULO'),
 )
 
-def p_variables(p):
-    """
-    variable : VARNAME
-    """
-    if(global_variables.has_key(p[1])):
-        p[0] = global_variables[p[1]]
+def p_assignment(p):
+    """statement : VARNAME EQUALS expression"""
+    global_variables[p[1]] = p[3]
+
+
+def p_statement(p):
+    """statement : expression"""
+    print(p[1])
 
 
 def p_expression(p):
-    '''expression : expression PLUS expression
+    """
+        expression : expression PLUS expression
             | expression MINUS expression
-            | expression TIMES expression
+            | expression MULT expression
             | expression DIVIDE expression
             | expression MODULO expression
-            | LPAREN expression RPAREN
-            | NUMBER '''
+    """
     if p[2] == '+':
         p[0] = p[1] + p[3]
     elif p[2] == '-':
@@ -39,11 +41,24 @@ def p_expression(p):
         p[0] = p[1] % p[3]
 
 
-def p_program(p):
-    """
-        program : expression
-    """
+def p_expression_Paren(p):
+    """expression : LPAREN expression RPAREN"""
+    p[0] = p[2]
+
+
+def p_expression_number(p):
+    """expression : NUMBER"""
     p[0] = p[1]
+
+
+def p_expression_var(p):
+    """expression : VARNAME"""
+    try:
+        p[0] = global_variables[p[1]]
+    except LookupError:
+        print("Undefined ID '%s'" % p[1])
+        p[0] = 0
+
 
 
 #def p_assignment(p):
@@ -54,17 +69,18 @@ def p_program(p):
     #p[0] = p[3]
     #print (global_variables)
 
-def p_factor(p):
-    """
-    factor : INT_CONST
-        | LPAREN expression RPAREN
-        | variable
-    """
-    if len(p) == 4:
-        p[0] = p[2]
-    else:
-        p[0] = int(p[1])
+#def p_factor(p):
+#    """
+#    factor : INT_CONST
+#        | LPAREN expression RPAREN
+#        | variable
+#    """
+#    if len(p) == 4:
+#        p[0] = p[2]
+#    else:
+#        p[0] = int(p[1])
 
 def p_error(p):
-    print("Syntax error")
+    print("Syntax error\n")
+
 parser = yacc.yacc()
